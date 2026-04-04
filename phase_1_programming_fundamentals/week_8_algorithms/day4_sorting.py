15beac647acb4ef2df3d17add7e440cfd803ea89
MD Zeeshan <mdzeeshan23707@gmail.com>
2026-04-04 19:07:40 +0530

    Week 8 Day 4:Sorting completed

---
+++ phase_1_programming_fundamentals/week_8_algorithms/day4_sorting.py
@@ -0,0 +1,27 @@
+# 1. Bubble sort
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+arr = [3, 2, 1, 5, 4]
+result = bubble_sort(arr)
+print(result)
+
+# 3. Selection sort
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+
+arr = [2, 4, 1, 3, 5]
+result = selection_sort(arr)
+print(result)
