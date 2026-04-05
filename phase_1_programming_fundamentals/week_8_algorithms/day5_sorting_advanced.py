bacd0ad5032511772b773217a395638c775fb64f
MD Zeeshan <mdzeeshan23707@gmail.com>
2026-04-05 20:27:00 +0530

    Week 8 Day 5:Sorting advanced completed

---
+++ phase_1_programming_fundamentals/week_8_algorithms/day5_sorting_advanced.py
@@ -0,0 +1,45 @@
+# 1. Merge Sort
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
+arr = [5, 2, 9, 1, 3]
+print(merge_sort(arr))
+
+# 2. Quick Sort
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    pivot = arr[len(arr) // 2]
+
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+
+    return quick_sort(left) + middle + quick_sort(right)
+
+arr = [5, 2, 9, 1, 3]
+print(quick_sort(arr))
