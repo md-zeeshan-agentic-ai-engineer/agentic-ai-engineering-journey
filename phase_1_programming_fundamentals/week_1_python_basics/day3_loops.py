e4b8b1aec60118b48e0f951d01a3d42e5e51f717
MD Zeeshan <mdzeeshan23707@gmail.com>
2026-07-18 17:14:33 +0530

    Updated day3 loops examples

--- phase_1_programming_fundamentals/week_1_python_basics/day3_loops.py
+++ phase_1_programming_fundamentals/week_1_python_basics/day3_loops.py
@@ -1,12 +1,8 @@
 #####################               for loop             ####################
 
-#Basic syntax
-#for variable in sequence:
-#    code
-#Example:
 for i in range(5):
     print(i)
-range()
+
 for i in range(1, 6):
     print(i)
 for i in range(1, 10, 2):
@@ -14,58 +10,45 @@ for i in range(1, 10, 2):
 for i in range(3):
     print("Hello")
 
-#Example 1: 1 se 10 print karo
+# Example 1: Print 1-10
 for i in range(1, 11):
    print(i)
 
-#Example 2: Even numbers (1–20)
+# Example 2: Even numbers (1–20)
 for i in range(2, 21, 2):
     print(i)
 
-#Example 3: Table of 5
+# Example 3: Table of 5
 for i in range(1, 11):
     print(5 * i)
 
 ###############               while loop               ###############
 
-#Basic Syntax
-#while condition:
-#   code
-
-
-#Output:
-1
-2
-3
-4
-5
-
-#Step-by-step samjhiye
+# Understand step-by-step
 i = 1
 while i <= 3:
     print("Hello")
     i += 1
 
 
-#i = 1 → condition true → print
-#i = 2 → condition true → print
-#i = 3 → condition true → print
-#i = 4 → condition false → loop stop
+# i = 1 → condition true → print
+# i = 2 → condition true → print
+# i = 3 → condition true → print
+# i = 4 → condition false → loop stop
 
-#Common Examples
-#Example 1: 1 se 10 print
+# Example 1: Print 1-10
 i = 1
 while i <= 10:
     print(i)
     i += 1
 
-#Example 2: Even numbers (1–20)
+# Example 2: Even numbers (1–20)
 i = 2
 while i <= 20:
     print(i)
     i += 2
 
-#Example 3: User se input jab tak sahi na ho
+# Example 3: User se input jab tak sahi na ho
 password = ""
 
 while password != "python":
@@ -74,25 +57,25 @@ while password != "python":
 print("Access granted")
 
 ####################            #Pattern printer                 ####################
-#Pattern 1: Simple Star Pattern
+# Pattern 1: Simple Star Pattern
 for i in range(1,5):
     for j in range(i):
         print("*",end="")
     print()
 
-#Pattern 2: Number Pattern
+# Pattern 2: Number Pattern
 for i in range(1,5):
     for j in range(1,i+1):
         print("*",end="")
     print()
 
-#Pattern 3: Reverse Star Pattern
+# Pattern 3: Reverse Star Pattern
 for i in range(4,0,-1):
     for j in range(i):
         print("*",end="")
     print()
 
-#Pattern 4: Square Pattern
+# Pattern 4: Square Pattern
 for i in range(4):
     for j in range(4):
         print("*", end="")
